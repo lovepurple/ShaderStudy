9952bfe342a05269e08426ebc8160e789bfd6302
lovepurple-ourpalm <songguangze@outlook.com>
2017-09-27 15:49:00 +0800

    planar shadow on arbitraryplane

---
+++ Assets/Resources/Shader/Resources/Shader-Shadow/PlanarShadowOnCustomPlanar.shader
@@ -0,0 +1,61 @@
+﻿
+Shader "Custom/PlanarShadowOnCustomPlanar" {
+	Properties
+	{
+		_ShadowColor("Shadow Color",Color) = (0.5,0.5,0.5,0.7)
+		_ShadowBias("Shadow Bias",Range(0.1,0.99)) = 0.3
+	}
+		SubShader
+	{
+		pass
+		{
+			Tags{ "LightMode" = "ForwardBase"  "Queue" = "Opaque" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+			Blend SrcAlpha OneMinusSrcAlpha
+			ZWrite On
+
+			Stencil
+			{
+				Ref 0
+				Comp Equal
+				Pass IncrWrap
+				ZFail Keep
+			}
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			uniform float4x4 _World2Ground;
+			uniform float4x4 _Ground2World;
+
+			float4 _ShadowColor;
+			half _ShadowBias;
+
+			float4 vert(float4 vertex: POSITION) : SV_POSITION
+			{
+				float3 litDir;
+
+				litDir = WorldSpaceLightDir(vertex);
+				litDir = mul(_World2Ground,float4(litDir,0)).xyz;
+				litDir = normalize(litDir);
+				float4 vt;
+				vt = mul(unity_ObjectToWorld, vertex);
+				vt = mul(_World2Ground,vt);
+				vt.x = vt.x - (vt.y / litDir.y)*litDir.x;
+				vt.z = vt.z - (vt.y / litDir.y)*litDir.z;
+				vt.y = _ShadowBias;
+				vt = mul(_Ground2World,vt);
+				vt = mul(unity_WorldToObject,vt);
+				return UnityObjectToClipPos(vt);
+			}
+
+			float4 frag(void) : COLOR
+			{
+				return _ShadowColor;
+			}
+			ENDCG
+	}
+	}
+}
---
+++ Assets/Script/PlanarShadowOnArbitraryPlane.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 场景接受PlanarShadow面
+/// </summary>
+[Serializable]
+public class PlanarShadowOnArbitraryPlane : MonoBehaviour
+{
+    //阴影颜色
+    [SerializeField]
+    public Color ShadowColor = new Color(0.5f, 0.5f, 0.5f, 0.8f);
+
+    //阴影物体父节点
+    [SerializeField]
+    public Transform CastShadowParentTransform = null;
+
+    [SerializeField]
+    public float ShadowOffset = 0.4f;
+
+    private HashSet<Material> m_shadowMaterialSet = new HashSet<Material>();
+    private Material m_defaultShadowMaterial = null;
+    private Shader m_shadowShader = null;
+
+    void Awake()
+    {
+        CreateDefaultShadowMaterial();
+    }
+
+
+    void OnEnable()
+    {
+        ResetCastShadowMaterials();
+    }
+
+    void Update()
+    {
+        if (m_defaultShadowMaterial)
+        {
+            m_defaultShadowMaterial.SetColor("_ShadowColor", ShadowColor);
+            m_defaultShadowMaterial.SetFloat("_ShadowBias", ShadowOffset);
+        }
+    }
+
+    /// <summary>
+    /// Initial or Reset all Shadow material for children
+    /// </summary>
+    private void ResetCastShadowMaterials()
+    {
+        if (!CastShadowParentTransform)
+            CastShadowParentTransform = transform;
+
+        m_shadowMaterialSet.Clear();
+
+        for (int i = 0; i < CastShadowParentTransform.childCount; ++i)
+        {
+            Transform childTransform = CastShadowParentTransform.GetChild(i);
+            Renderer[] childRenderers = childTransform.GetComponentsInChildren<Renderer>(true);
+            if (childRenderers == null)
+                continue;
+
+            for (int k = 0; k < childRenderers.Length; ++k)
+            {
+                Renderer childRenderer = childRenderers[k];
+
+                bool renderHaveShadowMaterial = false;
+                Material[] renderMaterials = childRenderer.sharedMaterials;
+                for (int j = 0; j < renderMaterials.Length; ++j)
+                {
+                    Material renderMaterial = renderMaterials[j];
+                    if (!renderMaterial || !renderMaterial.shader)
+                        continue;
+
+                    if (renderMaterial.shader.name == m_defaultShadowMaterial.name)
+                    {
+                        m_shadowMaterialSet.Add(renderMaterial);
+                        renderHaveShadowMaterial = true;
+                    }
+                }
+
+                if (!renderHaveShadowMaterial)
+                {
+                    List<Material> renderAddShadowMaterial = new List<Material>();
+                    renderAddShadowMaterial.AddRange(renderMaterials);
+                    renderAddShadowMaterial.Add(m_defaultShadowMaterial);
+                    childRenderer.sharedMaterials = renderAddShadowMaterial.ToArray();
+
+                    m_shadowMaterialSet.Add(m_defaultShadowMaterial);
+                }
+            }
+
+        }
+
+        foreach (Material shadowMat in m_shadowMaterialSet)
+        {
+            shadowMat.SetMatrix("_World2Ground", transform.worldToLocalMatrix);
+            shadowMat.SetMatrix("_Ground2World", transform.localToWorldMatrix);
+        }
+    }
+
+    /// <summary>
+    /// Children Changed then reset Shadow
+    /// </summary>
+    private void OnTransformChildrenChanged()
+    {
+        ResetCastShadowMaterials();
+    }
+
+    /// <summary>
+    /// Create Default PlanarShadow material for children Renders
+    /// </summary>
+    /// <returns></returns>
+    public Material CreateDefaultShadowMaterial()
+    {
+        if (!m_shadowShader)
+        {
+            m_shadowShader = Shader.Find("Custom/PlanarShadowOnCustomPlanar");
+            if (!m_shadowShader)
+            {
+                Debug.LogError("Custom/PlanarShadowOnCustomPlanar 不存在");
+                return null;
+            }
+        }
+
+        m_defaultShadowMaterial = new Material(m_shadowShader);
+        m_defaultShadowMaterial.SetColor("_ShadowColor", ShadowColor);
+        m_defaultShadowMaterial.SetFloat("_ShadowBias", ShadowOffset);
+
+        return m_defaultShadowMaterial;
+    }
+}
