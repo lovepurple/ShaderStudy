430ab99309dbe6e3b36e1933a885eed1f6067049
lovepurple-ourpalm <songguangze@outlook.com>
2017-10-11 11:51:28 +0800

    sample cameradepthnormaltexture

---
+++ Assets/Resources/Shader/Shader-Depth/DepthNormalTexture.shader
@@ -0,0 +1,92 @@
+﻿Shader "Hidden/DepthNormalTexture"
+{
+	Properties
+	{
+
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 100
+		ZWrite Off
+		ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile SHOW_DEPTHNORMAL_NORMAL_ON SHOW_DEPTHNORMAL_NORMAL_OFF
+			#pragma multi_compile SHOW_DEPTHNORMAL_DEPTH_ON SHOW_DEPTHNORMAL_DEPTH_OFF
+			#pragma multi_compile SHOW_DEPTH_ON SHOW_DEPTH_OFF
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+
+			uniform sampler2D _CameraDepthTexture;
+			uniform sampler2D _CameraDepthNormalsTexture;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float2 screenPos:TEXCOORD1;
+			};
+
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				o.screenPos = ComputeScreenPos(o.vertex);
+
+				#if UNITY_UV_STARTS_AT_TOP
+					if (_MainTex_TexelSize.y < 0.0)
+						o.uv.y = 1.0 - o.uv.y;
+				#endif
+
+
+				return o;
+			}
+
+			float4 frag(v2f i) : SV_Target
+			{
+				float depthValue;
+				float3 viewNormalValues;		//ViewNormal
+
+				//float3 nn = tex2D(_CameraDepthNormalsTexture, i.uv).xyz * float3(3.5554, 3.5554, 0) + float3(-1.7777, -1.7777, 1.0);
+				//float g = 2.0 / dot(nn.xyz, nn.xyz);
+				//float3 normal = float3(g * nn.xy, g - 1.0); // View space normal
+				DecodeDepthNormal(tex2D(_CameraDepthNormalsTexture, i.screenPos), depthValue, viewNormalValues);
+
+				float depthFromDepthTexture = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+
+				float4 col = float4(1, 1, 1, 1);
+
+				#if SHOW_DEPTHNORMAL_NORMAL_ON
+					col.rgb = viewNormalValues;
+					return col;
+				#elif SHOW_DEPTHNORMAL_DEPTH_ON
+					col.rgb = depthValue;
+					return col;
+				#elif SHOW_DEPTH_ON
+					col.rgb = Linear01Depth(depthFromDepthTexture);
+					return col;
+				#endif
+
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Script/DepthNormalTexture.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class DepthNormalTexture : MonoBehaviour
+{
+    public DisplayTextureMode DisplayType = DisplayTextureMode.DepthNormal_Normal;
+
+    private Material depthMat;
+
+    void OnEnable()
+    {
+        CreateMaterialIfNeed();
+
+        //必须强制打开，否则不渲染深度图
+        Camera.main.depthTextureMode |= DepthTextureMode.DepthNormals;
+        Camera.main.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    private void CreateMaterialIfNeed()
+    {
+        if (depthMat == null)
+            depthMat = new Material(Shader.Find("Hidden/DepthNormalTexture"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (depthMat != null)
+        {
+            List<string> shaderKeywords = new List<string>();
+
+            switch (DisplayType)
+            {
+                case DisplayTextureMode.Depth:
+                    shaderKeywords.Add("SHOW_DEPTH_ON");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_DEPTH_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_NORMAL_OFF");
+                    break;
+                case DisplayTextureMode.DepthNormal_Depth:
+                    shaderKeywords.Add("SHOW_DEPTH_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_NORMAL_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_DEPTH_ON");
+                    break;
+                case DisplayTextureMode.DepthNormal_Normal:
+                    shaderKeywords.Add("SHOW_DEPTH_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_NORMAL_ON");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_DEPTH_OFF");
+                    break;
+                default:
+                    shaderKeywords.Add("SHOW_DEPTH_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_NORMAL_OFF");
+                    shaderKeywords.Add("SHOW_DEPTHNORMAL_DEPTH_OFF");
+                    break;
+            }
+
+            //使用宏开关Shader中的状态
+            depthMat.shaderKeywords = shaderKeywords.ToArray();
+
+            Graphics.Blit(source, destination, depthMat);
+        }
+    }
+
+    public enum DisplayTextureMode
+    {
+        DepthNormal_Normal,
+        DepthNormal_Depth,
+        Depth,
+        None
+    }
+}
+
