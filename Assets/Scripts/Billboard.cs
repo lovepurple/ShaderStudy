adf3b11e73f70047e2a2a23405df8a969eeb3507
lovepurple-ourpalm <songguangze@outlook.com>
2017-11-07 15:41:09 +0800

    billboard 适应模型自身缩放

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Material mat;
+
+    private void Update()
+    {
+        if (mat == null)
+            mat = GetComponentInChildren<Renderer>().material;
+
+        if (mat == null)
+            return;
+
+        //模型自身缩放
+        mat.SetMatrix("_ObjectScaleMatrix", Matrix4x4.Scale(transform.localScale));
+
+    }
+
+
+}
