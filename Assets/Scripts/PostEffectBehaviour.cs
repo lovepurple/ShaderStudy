d17753774716eed749d81beb951f36f44b0d0905
lovepurple <songguangze@outlook.com>
2018-05-21 11:29:29 +0800

    remove clear

---
+++ Assets/Scripts/PostEffectBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PostEffectBehaviour : MonoBehaviour
+{
+    public Material m_renderMateiral;
+
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (this.m_renderMateiral != null)
+            Graphics.Blit(src, dest, m_renderMateiral);
+        else
+            Graphics.Blit(src, dest);
+
+    }
+
+}
